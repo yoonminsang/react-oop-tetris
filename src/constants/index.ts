0d3b8a73666151caa3c9d60fd6e75dac8360eefd
yoonminsang <apdlyooapdl@naver.com>
2022-06-03 01:12:12 +0900

    feat: SET 상수 생성

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+export const SET = {
+  lastStage: 10,
+  minSpeed: 500,
+  maxSpeed: 1000,
+  row: 20,
+  col: 10,
+};
+
+export const KEY_EVENT = {
+  left: 'ArrowLeft',
+  right: 'ArrowRight',
+  up: 'ArrowUp',
+  down: 'ArrowDown',
+  space: 'Space',
+};
