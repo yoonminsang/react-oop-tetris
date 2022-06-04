246d78b2b31e687bb3b04411841b37e60745a547
yoonminsang <apdlyooapdl@naver.com>
2022-06-04 15:54:40 +0900

    feat: stageintro 렌더링테스트

---
+++ src/components/tetris/stage-intro.spec.tsx
@@ -0,0 +1,44 @@
+import { act, render } from '@testing-library/react';
+
+import StageIntro from './stage-intro';
+
+const renderComplex = (stageParam: number) => {
+  const setStep = jest.fn();
+  const increaseStage = jest.fn();
+  const { container } = render(<StageIntro setStep={setStep} stage={stageParam} increaseStage={increaseStage} />);
+  const stage = Number(container.querySelector('b')?.textContent);
+  const time = () => Number(container.querySelector('h2')?.textContent);
+  return { setStep, increaseStage, stage, time, container };
+};
+
+beforeEach(() => {
+  jest.useFakeTimers();
+  jest.spyOn(window, 'setInterval');
+});
+
+describe('<StageIntro />', () => {
+  it('처음 stage, time', () => {
+    const stageParam = 1;
+    const { stage, time, increaseStage } = renderComplex(stageParam);
+    expect(increaseStage).toBeCalled();
+    expect(stage).toBe(stageParam);
+    expect(time()).toBe(3);
+  });
+  it('3초 타이머후 step 변경', () => {
+    const { setStep, time } = renderComplex(1);
+    expect(time()).toBe(3);
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    expect(time()).toBe(2);
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    expect(time()).toBe(1);
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    expect(time()).toBe(0);
+    expect(setStep).toBeCalled();
+  });
+});
