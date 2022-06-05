24401c9885145e45f1bee0915f8df5de58355d5e
yoonminsang <apdlyooapdl@naver.com>
2022-06-05 20:49:53 +0900

    feat: usePlayStatus 훅 생성

---
+++ src/hooks/use-play-table.ts
@@ -0,0 +1,48 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { IBlock, makeBlockToTable, makeEmptyTable } from '@/utils/block.util';
+import { copy2DArr } from '@/utils';
+
+export const usePlayTable = (
+  position: { x: number; y: number },
+  isCrash: boolean,
+  currentBlock: IBlock,
+  onInitUser: () => void,
+) => {
+  const [table, setTable] = useState<string[][]>(makeEmptyTable());
+  const [viewTable, setViewTable] = useState<string[][]>(table);
+  const [clearLine, setClearLine] = useState<number>(0);
+
+  const changeClearLine = useCallback((table: string[][]) => {
+    const newTable = copy2DArr(table);
+    let line = 0;
+    newTable.forEach((row, index) => {
+      const isClear = !row.includes('0');
+      if (isClear) {
+        line += 1;
+        for (let i = index; i > 0; i--) {
+          newTable[i] = newTable[i - 1];
+        }
+      }
+    });
+    setClearLine(line);
+    return newTable;
+  }, []);
+
+  useEffect(() => {
+    const updateTable = (table: string[][]) => {
+      const newTable = makeBlockToTable(table, currentBlock, position);
+      setViewTable(newTable);
+      if (isCrash) {
+        onInitUser();
+        return changeClearLine(newTable);
+      }
+      return newTable;
+    };
+
+    setClearLine(0);
+    setTable((table) => updateTable(table));
+  }, [currentBlock, isCrash, onInitUser, position, changeClearLine]);
+
+  return { viewTable, clearLine };
+};
