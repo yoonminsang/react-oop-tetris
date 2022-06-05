ef4a165a36328a522a7cc0d9e99bc85ae6790edf
yoonminsang <apdlyooapdl@naver.com>
2022-06-06 00:37:17 +0900

    feat: play 컴포넌트 1차 완성
    - space로 맨 밑으로 블록이동
    - 아래버튼으로 버튼을 이동시킬때 timer 조작
    - 블록 회전 적용
    - 기타 코드정리

--- src/components/tetris/play/play.tsx
+++ src/components/tetris/play/play.tsx
@@ -3,11 +3,10 @@
 /* eslint-disable react/no-array-index-key */
 
 import { css } from '@emotion/react';
-import { Dispatch, FC, SetStateAction, useCallback, useEffect } from 'react';
+import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
 
 import { TPanel } from '@/types';
-import { KEY_EVENT, SET } from '@/constants';
-import { Block } from '@/utils/block.util';
+import { KEY_EVENT } from '@/constants';
 import { useInterval } from '@/hooks/use-interval';
 import { usePlayStatus } from '@/hooks/use-play-status';
 import { usePlayUser } from '@/hooks/use-play-user';
@@ -32,18 +31,43 @@ const WrapperS = css`
 `;
 
 const Play: FC<Props> = ({ setStep, stage, score, setScore }) => {
-  const { position, isCrash, onUpdatePosition, currentBlock, nextBlock, initUser } = usePlayUser();
+  const { position, isCrash, onUpdatePosition, currentBlock, nextBlock, initUser, blockRotate } = usePlayUser();
   const { table, viewTable, clearLine } = usePlayTable(position, isCrash, currentBlock, initUser);
   const { speed, needClearLine } = usePlayStatus(stage, setScore, clearLine);
+  const [dropTime, setDropTime] = useState<number | null>(speed);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const moveBlock = (x: number, y: number) => {
+    if (checkMoveBlock(currentBlock, table, position, { x, y })) {
+      onUpdatePosition({ x, y }, false);
+      return true;
+    }
+    return false;
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const drop = () => {
+    setDropTime(speed);
+    if (needClearLine <= 0) {
+      setStep('stageClear');
+    }
+
+    if (!moveBlock(0, 1)) {
+      if (position.y < 1) {
+        setStep('dead');
+      }
+      onUpdatePosition({ x: 0, y: 0 }, true);
+      return false;
+    }
+    return true;
+  };
+
+  useInterval(() => {
+    drop();
+  }, dropTime);
 
   useEffect(() => {
-    const moveBlock = (x: number, y: number) => {
-      if (checkMoveBlock(currentBlock, table, position, { x, y })) {
-        onUpdatePosition(x, y, false);
-      }
-    };
-
-    const fn = (e: KeyboardEvent) => {
+    const onKeyDown = (e: KeyboardEvent) => {
       switch (e.key) {
         case KEY_EVENT.left:
           moveBlock(-1, 0);
@@ -52,19 +76,36 @@ const Play: FC<Props> = ({ setStep, stage, score, setScore }) => {
           moveBlock(1, 0);
           break;
         case KEY_EVENT.up:
+          blockRotate(table, currentBlock, 1);
           break;
         case KEY_EVENT.down:
-          onUpdatePosition(0, +1, false);
+          setDropTime(null);
+          drop();
           break;
         case KEY_EVENT.space:
+          let y = 1;
+          while (checkMoveBlock(currentBlock, table, position, { x: 0, y })) {
+            y++;
+          }
+          onUpdatePosition({ x: 0, y: y - 1 }, true);
           break;
         default:
           break;
       }
     };
-    window.document.body.addEventListener('keydown', fn);
-    return () => window.document.body.removeEventListener('keydown', fn);
-  }, [currentBlock, onUpdatePosition, position, table]);
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === KEY_EVENT.down) {
+        setDropTime(speed * 0.5);
+      }
+    };
+    window.document.body.addEventListener('keydown', onKeyDown);
+    window.document.body.addEventListener('keyup', onKeyUp);
+    return () => {
+      window.document.body.removeEventListener('keydown', onKeyDown);
+      window.document.body.removeEventListener('keydown', onKeyUp);
+    };
+  }, [blockRotate, currentBlock, drop, moveBlock, onUpdatePosition, position, speed, table]);
+
   return (
     <div css={WrapperS}>
       <PlayAside score={score} needClearLine={needClearLine} stage={stage} />
